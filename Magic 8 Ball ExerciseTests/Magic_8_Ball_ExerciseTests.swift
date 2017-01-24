b9b3b07fc81f2b26ab8677180e228afdcefa590b
Jared Annibale <jaredannibale@jannibalembp.bc.edu>
2017-01-24 11:58:22 -0500

    Initial Commit

---
+++ Magic 8 Ball ExerciseTests/Magic_8_Ball_ExerciseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Magic_8_Ball_ExerciseTests.swift
+//  Magic 8 Ball ExerciseTests
+//
+//  Created by Jared Annibale on 1/24/17.
+//  Copyright © 2017 Jared Annibale. All rights reserved.
+//
+
+import XCTest
+@testable import Magic_8_Ball_Exercise
+
+class Magic_8_Ball_ExerciseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
