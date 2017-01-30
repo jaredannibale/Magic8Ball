eea0bcee65a6ad4287bdfa7fdef508dd16c944b2
Jared Annibale <jaredannibale@jannibalembp.bc.edu>
2017-01-30 17:25:31 -0500

    Simple Magic 8 Ball app

--- Magic 8 Ball Exercise/ViewController.swift
+++ Magic 8 Ball Exercise/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var showAnswerButton: UIButton!
 
+    var lastIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAnswerButtonPressed(_ sender: Any) {
+    
+        let answers = ["It is certain.",
+                       "It is decidedly so.",
+                       "Yes, definitely.",
+                       "You may rely on it.",
+                       "Outlook good.",
+                       "Without a doubt.",
+                       "As I see it, yes.",
+                       "Yes.",
+                       "Most likely.",
+                       "Signs point to yes.",
+                       "Reply hazy, try again.",
+                       "Ask again later.",
+                       "Better not tell you now.",
+                       "Cannot predict now.",
+                       "Concentrate and ask again.",
+                       "Don't count on it.",
+                       "My reply is no.",
+                       "Outlook not so good.",
+                       "Very doubtful.",
+                       "My sources say no."]
 
+        var randomIndex: Int
+        
+        repeat {
+            randomIndex = Int(arc4random_uniform(UInt32(answers.count)))
+        } while randomIndex == lastIndex
+        
+        lastIndex = randomIndex
+        
+        answerLabel.text = answers[randomIndex]
+        
+        
+    }
 }
 
